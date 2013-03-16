19f5351da2cbf1db498c26b4234e0b9eda02f78e
Bryan P. White <bpcwhite@gmail.com>
2013-03-15 20:09:30 -0700

    genbank converter is functional; updated installer

--- installer.pl
+++ installer.pl
@@ -25,30 +25,6 @@ print "Detecting operating system...$^O\n";
 if($^O =~ m/MSWin32/) {
 	print "Found ".$^O."\n";
 	
-	# Download BioPerl from bioperl.org DIST
-	print "Downloading BioPerl...\n";
-	my $url = 'http://bioperl.org/DIST/BioPerl-1.6.1.zip';
-	my $file = 'BioPerl-1.6.1.zip';
-	if(-e $file) {
-		print "Already downloaded $file\n";
-	} else {
-		getstore($url,$file);
-	}
-	
-	# Get the clean file name, i.e. no file extension.
-	my @delimited_file_name = split(/\./,$file);
-	my $clean_file_name = $delimited_file_name[0].".".$delimited_file_name[1].".".$delimited_file_name[2];
-	if(-e "$FindBin::Bin/tmp/$clean_file_name") {
-		goto skip_extract;
-	}
-
-	# Extract BioPerl zip.
-	print "Extracting BioPerl to $FindBin::Bin/tmp/$clean_file_name...\n";
-	my $ae = Archive::Extract->new( archive => "$FindBin::Bin/$file" );
-	my $ok = $ae->extract(to => "$FindBin::Bin/tmp") or die $ae->error;
-
-	skip_extract:
-	
 	# Install CPAN modules.
 	print "Installing CPAN...\n";
 	system("perl -MCPAN -e install CPAN");
@@ -57,61 +33,10 @@ if($^O =~ m/MSWin32/) {
 	system("perl -MCPAN -e o conf commit");
 	system("perl -MCPAN -e install Module::Build");
 	system("perl -MCPAN -e install Test::Harness");
-	# Change directory to temp working directory to build BioPerl.
-	chdir("$FindBin::Bin/tmp/$clean_file_name");
-	# exit;
-	print "Building BioPerl...\n";
-	system("perl Build.PL");
-	# exit;
-	print "Testing BioPerl...\n";
-	system("perl Build test");
-	print "Installing BioPerl...\n";
-	system("perl Build install");
-	
+
 	print "Done!";
 	exit;
-	# print "Detecting distribution...\n";
-	# my @perl_version = `perl -v`;
-	# my $perl_distribution = '';
-	# foreach my $line (@perl_version) {
-		# if ($line =~ m/ActiveState/) {
-			# $perl_distribution = 'ActiveState';
-			# last;
-		# }
-		# if ($line =~ m/Strawberry/) {
-			# $perl_distribution = 'Strawberry';
-			# last;
-		# }
-	# }
-	# if ($perl_distribution eq 'ActiveState') {
-		# print `ppm install PPM-Repositories`;
-		# print "Installing BioPerl...\n";
-		# print `ppm repo add http://bioperl.org/DIST`;
-		# print `ppm repo add uwinnipeg`;
-		# print `ppm repo add trouchelle http://trouchelle.com/ppm12/`;
-		# print `ppm install BioPerl`;
-		# exit;
-		# print "Done.\n";
-		# print "Installing MinGW...\n";
-		# print `ppm install mingw`;
-		# print "Done.\n";
-		# print "Installing other modules...\n";
-		# print `ppm install Moose`;
-		# print `ppm install MooseX::ClassAttribute`;
-		# print `ppm install Params::Validate`;
-		# print `ppm install FindBin`;
-		# print `ppm install Statistics::Descriptive`;
-		# print `ppm install Benchmark`;
-		# print `ppm install POSIX`;
-		# print `ppm install File::Copy`;
-		# print "Done.\n";
-	# } elsif ($perl_distribution eq 'Strawberry') {
-		# print "Your version of Perl is not currently supported by this installer.\n";
-		# exit;
-	# } else {
-		# print "Your version of Perl is not currently supported by this installer.\n";
-		# exit;
-	# }
+
 } elsif ($^O =~ m/linux/) {
 	print "Found ".$^O."\n";
 	print "Your operating system is not supported by this installer.\n";
--- scripts/seq_convert_genbank.pl
+++ scripts/seq_convert_genbank.pl
@@ -16,7 +16,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 use FindBin;
 use lib "$FindBin::Bin/libs"; 
+use Time::HiRes qw( usleep );
 
+					  
 use Bio::TreeIO;
 use Bio::Tree::Tree;
 use Bio::Tree::TreeFunctionsI;
@@ -34,10 +36,15 @@ use Class::Inspector;
 
 use Data::Dumper;
 
-my $target_taxon = 'Tricorythodes';
+my $target_taxon = 'Ostreidae';
+my $search_options = 'AND COI[gene]';
 my $taxon_limit = 1;
-my $sequence_limit = 1;
+my $sequence_limit = 1000;
 my $user_email = 'bpcwhite@gmail.com';
+my $dlm = ',';
+my $endl = "\n";
+my $output_file = $target_taxon."_output.csv";
+my $sleep_time = 1000000; # microsends, pause between seconds
 
 ##############################################################################
 # Retrieve taxon ID
@@ -63,11 +70,11 @@ my $sequence_search = Bio::DB::EUtilities->new(-eutil    => 'esearch',
 											   -retmax  => $sequence_limit,
 											   -rettype => 'gb',
 											   -email   => $user_email,
-											   -term    => $sequence_term);
+											   -term    => $sequence_term.' '.$search_options);
 my @sequence_ids = $sequence_search->get_ids();
-foreach my $seq_id (@sequence_ids) {
-	print $seq_id."\n";
-}
+
+print "Found ".scalar(@sequence_ids)." sequences to download.\n";
+print "Downloading sequences...\n";
 my $sequence_fetch = Bio::DB::EUtilities->new( -eutil   => 'efetch',
 											   -db      => 'nucleotide',
 											   -rettype => 'gb',
@@ -75,105 +82,172 @@ my $sequence_fetch = Bio::DB::EUtilities->new( -eutil   => 'efetch',
 											   -id      => \@sequence_ids);
 my $sequence_file = 'seqs_'.$taxon_id.'.gb';
 $sequence_fetch->get_Response(-file => $sequence_file);
+print "Sequences downloaded to genbank format.\n";
 ##############################################################################
 
 ##############################################################################
+# Open genbank file into needed locations.
+open (GENBANK, '<'.$sequence_file);
+my @genbank = <GENBANK>;
+
 my $seqin = Bio::SeqIO->new(-file   => $sequence_file,
                             -format => 'genbank');
+##############################################################################
 
-my $dumper_file = "dumper.txt";
-
-# unlink $dumper_file;
-# open (DUMP, '>>'.$dumper_file);
+my @output_lines = ();
+push(@output_lines,
+	'taxon_id',$dlm,
+	'accession',$dlm,
+	'description',$dlm,
+	'gene',$dlm,
+	'product',$dlm,
+	'binom',$dlm,
+	'tax_hierarchy',$dlm,
+	'pub_title',$dlm,
+	'pub_authors',$dlm,
+	'pub_abstract_text',$dlm,
+	'jrn_name',$dlm,
+	'jrn_doi',$dlm,	
+	'jrn_so',$dlm,
+	'jrn_volume',$dlm,
+	'jrn_issue',$dlm,
+	'jrn_pages',$dlm,
+	'jrn_pubdate',$dlm,
+	'nuc_seq',$dlm,
+	'prot_seq',$dlm,
+	'primers',$dlm,
+	'codon_start',$dlm,
+	'collection_date',$dlm,
+	'voucher_id',$dlm,
+	'collected_by',$dlm,
+	'identified_by',$dlm,
+	'organelle',$dlm,
+	'location',$dlm,
+	'lat_lon',$dlm,
+	$endl);
 
 ##############################################################################
 # Loop through the downloaded genbank files and parse all the data
+my $seq_counter = 0;
 while (my $seq = $seqin->next_seq) {
+	print "Seq #: ".$seq_counter."\n";
+	usleep($sleep_time); 	# Sleep so you don't overload NCBI's servers.
 	# Pull these values as you go along and parse the genbank file.
-	my $taxon_id 			= undef; # Taxon ID for NCBI
-	my $accession_number	= undef; # NCBI accession number
-	my $publication_title 	= undef; # Most recent publication title
-	my $publication_authors	= ''; # Pub authors
-	my $long_name 			= ''; # Long description name for the sequence
-	# my $publication_title 	= '';
-	# my $publication_authors	= '';
-	# my $taxon_id 			= '';
-	# my $publication_title 	= '';
-	# my $publication_authors	= '';
-	my $abstract_text 			= undef;
-	print $seq->display_name."\n";
-	print $seq->description."\n";
-	print $seq->accession_number."\n";
+	# NCBI/Taxonomy Variables
+	my $taxon_id 				= 'NA'; # Taxon ID for NCBI
+	my $accession_number		= 'NA'; # NCBI accession number
+	my $long_name 				= 'NA'; # Long description name for the sequence
+	my $gene_name				= 'NA'; # Genbank gene name (e.g. COI)
+	my $product_name			= 'NA'; # Genbank product name (e.g. cytochrome oxidase I)
+	my $binomial_name			= 'NA';
+	my @hierarchical_taxonomy	= 'NA';
+	my $taxonomy_print_string 	= 'NA'; # Concatenated taxonomy string
+	# Reference Variables
+	my $publication_title 		= 'NA'; # Most recent publication title
+	my $publication_authors		= 'NA'; # Pub authors
+	my $abstract_text 			= 'NA'; # Abstract from pubmed article
+	my $journal_name			= 'NA'; # Reference journal name
+	my $journal_DOI				= 'NA'; # Online DOI access
+	my $journal_SO				= 'NA'; # Journal SO (pub info)
+	my $journal_volume			= 'NA'; # Journal volume #
+	my $journal_issue			= 'NA'; # Journal issue #
+	my $journal_pages			= 'NA'; # Journal page #'s
+	my $journal_pubdate			= 'NA'; # Journal pubdate
+	# Sequence Variables
+	my $nucleotide_seq			= 'NA'; # Nucleotide sequence
+	my $amino_acid_seq			= 'NA'; # Amino acid/protein sequence
+	my $codon_start				= 'NA'; # Reading frame position
+	my @pcr_primers				= 'NA'; # PCR primer list
+	my $pcr_primer_print_string = 'NA'; # Concatenated PCR primer string
+	my $protein_id				= 'NA'; # Protein NCBI ID
+	# Sample Information
+	my $collection_date			= 'NA'; # 
+	my $voucher_id				= 'NA'; # 
+	my $collected_by			= 'NA'; # 
+	my $identified_by			= 'NA'; # 
+	my $organelle				= 'NA'; # 
+	my $country					= 'NA'; # 
+	my $lat_lon					= 'NA'; # 
+
+	$long_name 			= $seq->description if defined $seq->description;
+	$accession_number 	= $seq->accession_number if defined $seq->accession_number;
+	$nucleotide_seq		= $seq->seq if defined $seq->seq;
 	
 	##############################################################################
 	# Obtain sequence feature information.
-	print "\nSequence Features\n\n";
-	for my $feat_object ($seq->get_SeqFeatures) {          
-	print "primary tag: ", $feat_object->primary_tag, "\n";          
-	for my $tag ($feat_object->get_all_tags) {             
-		  print "  tag: ", $tag, "\n";
-			
-		  for my $value ($feat_object->get_tag_values($tag)) {                
-			 print "    value: ", $value, "\n";
-			if($value =~ m/taxon:/) { 
-				my @taxon_split = split(/:/,$value);
-				$taxon_id = $taxon_split[1];
+	print "\tRetrieving sequence features...\n";
+	for my $feat_object ($seq->get_SeqFeatures) {
+		for my $tag ($feat_object->get_all_tags) {             			
+			for my $value ($feat_object->get_tag_values($tag)) {
+				next if !defined($value); # Make sure value is defined.
+				# print "    value: ", $value, "\n";
+				if($value =~ m/taxon:/) { my @taxon_split = split(/:/,$value); $taxon_id = $taxon_split[1] };
+				push(@pcr_primers, $value) 		 if($tag =~ m/PCR_primers/);
+				$collected_by 			= $value if($tag =~ m/collected_by/);
+				$collection_date 		= $value if($tag =~ m/collection_date/);
+				$country 				= $value if($tag =~ m/country/);
+				$identified_by			= $value if($tag =~ m/identified_by/);
+				$lat_lon				= $value if($tag =~ m/lat_lon/);
+				$organelle				= $value if($tag =~ m/organelle/);
+				$codon_start			= $value if($tag =~ m/codon_start/);
+				$gene_name				= $value if($tag =~ m/gene/);
+				$product_name			= $value if($tag =~ m/product/);
+				$protein_id				= $value if($tag =~ m/protein_id/);
+				$amino_acid_seq			= $value if($tag =~ m/translation/);
+				$voucher_id				= $value if($tag =~ m/specimen_voucher/);
 			}
-		  }          
-	   }       
+		}
 	}
 	##############################################################################
 	# Obtain reference annotations.
-	print "\nAnnotations\n\n";
+	print "\tRetrieving annotations...\n";
 	my $anno_collection = $seq->annotation;
 	for my $key ( $anno_collection->get_all_annotation_keys ) {
 		my @annotations = $anno_collection->get_Annotations($key);
 		for my $value ( @annotations ) {
-			print "tagname : ", $value->tagname, "\n";
-			  # $value is an Bio::Annotation, and also has an "as_text" method
-			print "  annotation value: ", $value->display_text, "\n";
 			if ($value->tagname eq "reference") {
-				if(defined($value->authors)) { print "author: ",$value->authors(), "\n"};
-				if(defined($value->title) && !defined($publication_title)) { 
+				# Store only the FIRST MOST RECENT reference.
+				if(defined($value->authors) && ($publication_authors eq 'NA')) { 
+					$publication_authors = $value->authors;
+				}				
+				if(defined($value->title) && ($publication_title eq 'NA')) { 
 					$publication_title = $value->title;
-					print "title: ",$value->title(), "\n";
-				} else {
-					print "title: ",$value->title(), "\n";
 				}
-
-				if(defined($value->medline)) { print "medline: ",$value->medline(), "\n"};
-				if(defined($value->editors)) { print "editors: ",$value->editors(), "\n"};
-				if(defined($value->database)) { print "database: ",$value->database(), "\n"};
-				if(defined($value->pubmed)) { print "pubmed: ",$value->pubmed(), "\n"};
-				if(defined($value->location)) {	print "location: ",$value->location(), "\n"};
-				if(defined($value->doi)) {	print "doi: ",$value->doi(), "\n"};
-				if(defined($value->gb_reference)) {	print "gb_reference: ",$value->location(), "\n"};
 			}
-				# my $hash_ref = $value->hash_tree;
-				# for my $key (keys %{$hash_ref}) {
-					# if(defined($hash_ref->{$key})) {
-						# print "\t".$key.": ".$hash_ref->{$key}."\n";
-					# }
-				# }
-			# }
 	   }
 	}
 	##############################################################################
 	
 	##############################################################################
 	# Get concise taxonomy information.
-	my $seq_taxonomy = Bio::DB::EUtilities->new(   -eutil    	=> 'esummary',
-												   -db      	=> 'taxonomy',
-												   -retmax  	=> 1,
-												   -email   	=> $user_email,
-												   -id    		=> $taxon_id);
-	my $ds = $seq_taxonomy->next_DocSum;
-	while (my $item = $ds->next_Item('flattened'))  {
-        # not all Items have content, so need to check...
-        printf("%-20s:%s\n",$item->get_name,$item->get_content) if $item->get_content;
-    }
-	# }
-	# my ($name)  = $seq_taxonomy->next_DocSum->get_contents_by_name('ScientificName');
+	my $found_organism_line = 0;
+	my $found_accession = 0;
+	my $organism_line_i = 0;
+	foreach my $genbank_line (@genbank) {
+		last if $genbank_line =~ m/REFERENCE/ && $found_organism_line == 1;
+		if($genbank_line =~ m/$accession_number/) { $found_accession = 1 };
+		if($genbank_line =~ m/ORGANISM/ && $found_accession == 1) { $found_organism_line = 1 };
+		if($found_organism_line == 1) {
+			# Parse the first line, which is the binomial name
+			if($organism_line_i == 0) {
+				$binomial_name = $genbank_line;
+				$binomial_name =~ s/ORGANISM//g; # Parse off the ORGANISM tag
+				$binomial_name =~ s/^\s+//; # remove leading spaces
+				$binomial_name =~ s/\s+$//; # remove trailing spaces
+				$binomial_name =~ s/ /_/g; 	# replace inner whitespace
+			} else {
+			# Parse the remaining lines which contain the hierarchical taxonomy
+				my $hierarchical_taxa_string = $genbank_line;
+				$hierarchical_taxa_string =~ s/^\s+//;	# remove leading spaces
+				$hierarchical_taxa_string =~ s/\s+$//;	# remove trailing spaces
+				$hierarchical_taxa_string =~ s/ //g; 	# replace inner whitespace
+				$hierarchical_taxa_string =~ s/\.//g; 	# delete periods
+				my @hierarchical_taxa_list = split(/;/,$hierarchical_taxa_string);
+				push(@hierarchical_taxonomy,@hierarchical_taxa_list);
+			}
+			$organism_line_i++;
+		}
+	}
 	##############################################################################
 	
 	##############################################################################
@@ -182,26 +256,33 @@ while (my $seq = $seqin->next_seq) {
 	# Extract the abstract text from the pubmed file.
 	my $cleaned_title = $publication_title;
 	$cleaned_title =~ s/://g; # Remove colons
-	# $cleaned_title =~ s/ /%20/g; # Replace whitespace with +'s
-
 	my $seq_pubmed = Bio::DB::EUtilities->new(   	-eutil    	=> 'esearch',
 												   -db      	=> 'pubmed',
 												   -retmax  	=> 1,
 												   -email   	=> $user_email,
-												   -term    	=> $cleaned_title);
-	# print $publication_title;
-	# print $seq_pubmed->get_term."\n";
+												   -term    	=> $cleaned_title,
+												   -verbose		=> -1);
+
 	my @pubmed_ids = $seq_pubmed->get_ids;
-	print $pubmed_ids[0]."\n";
 	my $seq_pubmed_summary = Bio::DB::EUtilities->new(   -eutil    	=> 'esummary',
-												   -db      	=> 'pubmed',
-												   -retmax  	=> 1,
-												   -email   	=> $user_email,
-												   -id    		=> $pubmed_ids[0]);
+														-db      	=> 'pubmed',
+														-retmax  	=> 1,
+														-email   	=> $user_email,
+														-id    		=> $pubmed_ids[0]);
+	print "\tCould not find article for: ".$cleaned_title."\n" 	if !defined($pubmed_ids[0]);
+	# Couldn't find at least one pubmed article, so skip this section
+	goto skip_pubmed 			if !defined($pubmed_ids[0]); 
+	# Did find pubmed articles, get the summary
 	my $ds_pubmed = $seq_pubmed_summary->next_DocSum;
 	while (my $item = $ds_pubmed->next_Item('flattened'))  {
-        # not all Items have content, so need to check...
-        printf("%-20s:%s\n",$item->get_name,$item->get_content) if $item->get_content;
+		next if !defined($item->get_content);
+		$journal_name = $item->get_content if $item->get_name =~ m/FullJournalName/;
+		$journal_DOI = $item->get_content if $item->get_name =~ m/DOI/;
+		$journal_SO = $item->get_content if $item->get_name =~ m/SO/;
+		$journal_volume = $item->get_content if $item->get_name =~ m/Volume/;
+		$journal_issue = $item->get_content if $item->get_name =~ m/Issue/;
+		$journal_pages = $item->get_content if $item->get_name =~ m/Pages/;
+		$journal_pubdate = $item->get_content if $item->get_name =~ m/PubDate/;
     }
 	my $pubmed_fetch = Bio::DB::EUtilities->new( -eutil   => 'efetch',
 												   -db      => 'pubmed',
@@ -218,22 +299,113 @@ while (my $seq = $seqin->next_seq) {
 			# Clear XML tags.
 			$abstract_text =~ s/\<AbstractText\>//g;
 			$abstract_text =~ s/\<\/AbstractText\>//g;
+			$abstract_text =~ s/^\s+//; # Remove leading space.
 		}
 	}
 	close(PUBMED);
 	unlink($pubmed_file);
+	skip_pubmed: # Skipped here because could not find any pubmed articles
 	##############################################################################
 	
-	##############################################################################
-	# print $name."\n";
-	print "\nSequence\n\n";
-	# print ">$name\n";
-	print $seq->seq."\n";
+	#############################################################################
+	# print "Taxon id: ".$taxon_id."\n"; 					# Taxon ID for NCBI
+	# print "Accession #: ".$accession_number."\n"; 		# NCBI accession number
+	# print "Description: ".$long_name."\n"; 				# Long description name for the sequence
+	# print "Gene name: ".$gene_name."\n"; 				# Genbank gene name (e.g. COI)
+	# print "Product name: ".$product_name."\n"; 			# Genbank product name (e.g. cytochrome oxidase I)
+	# print "Binomial name: ".$binomial_name."\n";	
+	# print "Full taxonomy: ";
+	foreach my $taxa_i (0 .. $#hierarchical_taxonomy) { # Print hierarchical taxonomy list
+		$taxonomy_print_string = '' if ($taxa_i == 0);
+		$taxonomy_print_string .= $hierarchical_taxonomy[$taxa_i].';' if ($taxa_i > 0);
+		# print $hierarchical_taxonomy[$taxa_i].';' if($taxa_i > 0);
+	}
+	# print "\n";
+	# Reference Variables
+	# print "Pub title: ".$publication_title."\n";   		# Most recent publication title
+	# print "Pub authors: ".$publication_authors."\n"; 	# Pub authors
+	# print "Abstract text: ".$abstract_text."\n"; 		# Abstract from pubmed article
+	# print "Journal name: ".$journal_name."\n";			# Reference journal name
+	# print "Journal DOI: ".$journal_DOI."\n";			# Online DOI access
+	# print "Journal SO: ".$journal_SO."\n";				# Journal SO (pub info)
+	# print "Journal volume: ".$journal_volume."\n";		# Journal volume #
+	# print "Journal issue: ".$journal_issue."\n";		# Journal issue #
+	# print "Journal pages: ".$journal_pages."\n";		# Journal page #'s
+	# print "Journal pubdate: ".$journal_pubdate."\n";	# Journal pubdate
+	# Sequence Variables
+	# print "Sequence: ".$nucleotide_seq."\n"; 			# Nucleotide sequence
+	# print "Protein: ".$amino_acid_seq."\n"; 			# Amino acid/protein sequence
+	# print "Primer list: ";
+	foreach my $primer_i (0 .. $#pcr_primers) { 		# PCR primer list
+		$pcr_primer_print_string = '' if ($primer_i == 0);
+		$pcr_primer_print_string .= $pcr_primers[$primer_i].';' if ($primer_i > 0);
+		# print "Set #$primer_i: ".$pcr_primers[$primer_i].';' if($primer_i > 0);
+	}
+	# print "\n";
+	# print "Codon start: ".$codon_start."\n";
+	# Sample Information
+	# print "Collection date: ".$collection_date."\n"; 	# 
+	# print "Voucher ID: ".$voucher_id."\n"; 				# 
+	# print "Collected by: ".$collected_by."\n"; 			# 
+	# print "Identified by: ".$identified_by."\n"; 		# 
+	# print "Organelle: ".$organelle."\n"; 				# 
+	# print "Location: ".$country."\n"; 					# 
+	# print "Lat\\Lon: ".$lat_lon."\n"; 					# 
+	#############################################################################
+	push(@output_lines,
+		$taxon_id,$dlm,
+		$accession_number,$dlm,
+		$long_name,$dlm,
+		$gene_name,$dlm,
+		$product_name,$dlm,
+		$binomial_name,$dlm,
+		$taxonomy_print_string,$dlm,
+		$publication_title,$dlm,
+		$publication_authors,$dlm,
+		$abstract_text,$dlm,
+		$journal_name,$dlm,
+		$journal_DOI,$dlm,	
+		$journal_SO,$dlm,
+		$journal_volume,$dlm,
+		$journal_issue,$dlm,
+		$journal_pages,$dlm,
+		$journal_pubdate,$dlm,
+		$nucleotide_seq,$dlm,
+		$amino_acid_seq,$dlm,
+		$pcr_primer_print_string,$dlm,
+		$codon_start,$dlm,
+		$collection_date,$dlm,
+		$voucher_id,$dlm,
+		$collected_by,$dlm,
+		$identified_by,$dlm,
+		$organelle,$dlm,
+		$country,$dlm,
+		$lat_lon,$dlm,
+		$endl);
+	# print "\nTaxonomy\n";
+	# print $binomial_name."\n";
+	# foreach my $taxa_i (0 .. $#hierarchical_taxonomy) {
+		# print $hierarchical_taxonomy[$taxa_i].';' if($taxa_i > 0);
+	# }
+	# print "\n";
+	# print "\nSequence\n\n";
+	# print ">$binomial_name\n";
+	# print $seq->seq."\n";
 	
-	print $abstract_text."\n";
+	# print "\nAbstract\n\n";
+	# print $abstract_text."\n";
 	##############################################################################
+	$seq_counter++;
+}
+
+unlink $output_file;
+open (OUTPUT, '>>'.$output_file);
+foreach my $output_line (@output_lines) {
+	print OUTPUT "\"".	$output_line if $output_line ne $endl;
+	print OUTPUT 		$output_line if $output_line eq $endl;
 
 }
+close(OUTPUT);
 ##############################################################################
 
 # Available database:

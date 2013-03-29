f9fa049704b008bf77dea6b75a0008715659d07d
Bryan P. White <bpcwhite@gmail.com>
2013-03-29 11:54:58 -0700

    created a bootstrap version of k2p function

--- scripts/libs/Sequence/Kimura_Distance_C.pl
+++ scripts/libs/Sequence/Kimura_Distance_C.pl
@@ -23,7 +23,7 @@ use warnings;
 use Exporter;
 
 our @ISA= qw( Exporter );
-our @EXPORT_OK = qw( c_kimura_distance );
+our @EXPORT_OK = qw(c_kimura_distance c_k2p_bootstrap);
 
 __C__
 
@@ -147,6 +147,132 @@ void c_kimura_distance(char* str1, char* str2, float critical_value,
 			else if(base_pair 	== 19){ // C->T
 					transversions++;
 					comparisons++;
+			}
+		}
+		if((i % 50) == 0) {
+			// Compute Kimura Distance
+			P = transitions/gene_length;
+			Q = transversions/gene_length;
+			w1 = 1-2*P-Q;
+			w2 = 1-2*Q;
+			c1 = 1/w1;
+			c2 = 1/w2;
+			c3 = (c1+c2)/2;
+			variance = pow(c1,2)*P+pow(c3,2)*Q-pow((c1*P+c3*Q),2);
+			stderror = sqrt(variance)/sqrt(gene_length);
+			K2P = -log(w1)/2-log(w2)/4;
+			// Compute minimum and maximum distance using normal distribution.
+			mink2p = K2P - critical_value*stderror;
+			if(mink2p < 0) {
+				mink2p = 0;
+			}
+			// maxk2p = K2P + critical_value*stderror;
+			
+			if(shortcut_type == 1) { // check strict K2P, Delete
+				if(K2P > cutoff)
+					goto cutoff_shortcut;
+			} else if(shortcut_type ==2) { // Check mink2p, Search
+				if(mink2p > cutoff)
+					goto cutoff_shortcut;
+			}
+		}
+	}
+	
+	// Compute Kimura Distance
+	P = transitions/comparisons;
+	Q = transversions/comparisons;
+	w1 = 1-2*P-Q;
+	w2 = 1-2*Q;
+	c1 = 1/w1;
+	c2 = 1/w2;
+	c3 = (c1+c2)/2;
+	variance = pow(c1,2)*P+pow(c3,2)*Q-pow((c1*P+c3*Q),2);
+	stderror = sqrt(variance)/sqrt(comparisons);
+	K2P = -log(w1)/2-log(w2)/4;
+	// Compute minimum and maximum distance using normal distribution.
+	mink2p = K2P - critical_value*stderror;
+	if(mink2p < 0) {
+		mink2p = 0;
+	}
+	maxk2p = K2P + critical_value*stderror;
+
+	cutoff_shortcut:
+
+	// Modify variables.
+	sv_setnv(sv_transitions,transitions);
+	sv_setnv(sv_transversions,transversions);
+	sv_setnv(sv_comparisons,comparisons);
+	sv_setnv(sv_K2P,K2P);
+	sv_setnv(sv_variance,variance);
+	sv_setnv(sv_stderror,stderror);
+	sv_setnv(sv_mink2p,mink2p);
+	sv_setnv(sv_maxk2p,maxk2p);
+}
+
+
+void c_k2p_bootstrap(char* str1, char* str2, float critical_value,
+						float cutoff, int shortcut_type, float gene_length,
+						SV* sv_transitions, SV* sv_transversions,
+						SV* sv_comparisons, SV* sv_K2P, SV* sv_variance,
+						SV* sv_stderror, SV* sv_mink2p, SV* sv_maxk2p,
+						char* bs_weights) {
+	/* Returns the following:
+		transitions
+		transversions
+		comparisons
+		K2P
+		variance
+		stderror
+		min (confidence intervals)
+		max (confidence intervals)
+		
+		XOR values for each transition/transversion
+			Transitions
+			A->G = 6
+			C->T = 23
+			Transversions
+			A->C = 2
+			A->T = 21
+			C->G = 4
+			G->T = 19
+	*/
+	float transitions = 0;
+	float transversions = 0;
+	float comparisons = 0;
+	short int i, base_pair = 0;
+	float 	P, Q, w1, w2, c1, c2, c3,
+			variance, stderror, K2P, 
+			mink2p, maxk2p = 0;
+	for(i = 0; i < strlen(str1); i++) {
+		if(str1[i] == str2[i]) {
+			// Skip identicals.
+			comparisons += bs_weights[i];
+		} else {
+			base_pair = str1[i] ^ str2[i];
+			// Transitions
+			if(base_pair 		== 6) { // A->G
+				transitions += bs_weights[i];
+				comparisons += bs_weights[i];
+			}
+			else if(base_pair 	== 23){ // C->T	
+				transitions += bs_weights[i];
+				comparisons += bs_weights[i];
+			} // Transversions
+			else if(base_pair 	== 21){ // A->T
+					transversions += bs_weights[i];
+					comparisons += bs_weights[i];
+			}
+			else if(base_pair 	== 2){ 	// A->C
+					transversions += bs_weights[i];
+					comparisons += bs_weights[i];
+			}
+			else if(base_pair 	== 4){ 	// C->G
+					transversions += bs_weights[i];
+					comparisons += bs_weights[i];
+			}
+			else if(base_pair 	== 19){ // C->T
+					transversions += bs_weights[i];
+					comparisons += bs_weights[i];
 			}
 		}
 		if((i % 50) == 0) {

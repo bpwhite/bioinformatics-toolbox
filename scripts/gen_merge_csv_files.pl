eb68e3f56d3891e2065125133f3396b536e0d407
Bryan P. White <bpcwhite@gmail.com>
2013-04-12 15:41:34 -0700

    added a script to merge CSV files that are output from the genbank downloader.

---
+++ scripts/gen_merge_csv_files.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+# Merges all CSV files in a directory.
+#
+# Copyright (c) 2013, Bryan White, bpcwhite@gmail.com
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+use General::Arguments;
+
+my $params = General::Arguments->new(	arguments_v => \@ARGV,
+									option_defs => {'-folder' 		=> '', 			# Folder where CSV files are stored.
+													}
+													);
+
+my $folder = $params->options->{'-folder'};
+												
+my @csv_files = <$folder/*.csv>;
+my @output_csv = ();
+foreach my $csv_file (@csv_files) {
+	print "Adding $csv_file\n";
+	open F, "< $csv_file";
+	my @current_file = <F>;
+	close F or die "Couln't close $csv_file!";
+	my $line_counter = 0;
+	foreach my $line (@current_file) {
+		if($line_counter == 0) { # skip first line
+			$line_counter = 1;
+			next;
+		}
+		push(@output_csv, $line);
+	}
+}
+
+my $output_file = 'combined.csv';
+unlink ($output_file);
+open (OUTPUT, '>>'.$output_file) or die "Could not open $f";
+foreach my $line(@output_csv) {
+	print OUTPUT $line;
+}
+close(OUTPUT);
+
+print 'Done combining!'."\n";

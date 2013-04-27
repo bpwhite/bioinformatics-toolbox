e6b3aefb3c6b179549226b9b648407fc9392ab58
Bryan P. White <bpcwhite@gmail.com>
2013-04-27 12:44:54 -0700

    added non-bootstrap kimura distance

--- scripts/libs/Sequence/Kimura_Distance.pm
+++ scripts/libs/Sequence/Kimura_Distance.pm
@@ -19,58 +19,7 @@ use warnings;
 use Exporter;
 
 our @ISA= qw( Exporter );
-our @EXPORT_OK = qw(k2p_bootstrap);
-
-sub k2p_bootstrap_old {
-	my $seq1 		= shift;
-	my $seq2 		= shift;
-	my $length 		= shift;
-	my @weights_arr = @_;
-	
-	my %lookup_table = (
-						# Transitions
-						'AG'	=> 'S',
-						'GA'	=> 'S',
-						'CT'	=> 'S',
-						'TC'	=> 'S',
-						# Transversions
-						'AC'	=> 'V',
-						'CA'	=> 'V',
-						'AT'	=> 'V',
-						'TA'	=> 'V',
-						'CG'	=> 'V',
-						'GC'	=> 'V',
-						'GT'	=> 'V',
-						'TG'	=> 'V',
-						# Identical
-						'AA'	=> 'I',
-						'GG'	=> 'I',
-						'CC'	=> 'I',
-						'TT'	=> 'I'
-					);
-	my %results = (
-					'S' => 0,
-					'V' => 0,
-					'I' => 0
-					);
-	my $i = 0;
-	while ($i < $length) {
-		my $pair = $lookup_table{substr($seq1, $i,1).substr($seq2, $i,1)};
-		$results{$pair} += $weights_arr[$i] if defined $pair;
-		$i++;
-	}
-	my $transitions 		= $results{'S'};
-	my $transversions 		= $results{'V'};
-	my $num_comparisons 	= $transitions + $transversions + $results{'I'};
-	my $P = $transitions/$num_comparisons;
-	my $Q = $transversions/$num_comparisons;
-	my $w1 = 1-2*$P-$Q;
-	my $w2 = 1-2*$Q;
-	my $K2P = -log($w1)/2-log($w2)/4;
-
-	return($K2P, $transitions, $transversions, $num_comparisons);
-}
-
+our @EXPORT_OK = qw(k2p_bootstrap k2p_no_bs);
 
 sub k2p_bootstrap {
 	my $seq1 		= shift;
@@ -123,6 +72,70 @@ sub k2p_bootstrap {
 			elsif($pair 	== 19){ # C->T
 				$transversions += @{$weights}[$i];
 				$num_comparisons += @{$weights}[$i];
+			}
+		}
+	}
+	
+	my $P = $transitions/$num_comparisons;
+	my $Q = $transversions/$num_comparisons;
+	my $w1 = 1-2*$P-$Q;
+	my $w2 = 1-2*$Q;
+	my $K2P = -log($w1)/2-log($w2)/4;
+
+	return($K2P, $transitions, $transversions, $num_comparisons);
+}
+
+
+sub k2p_no_bs{
+	my $seq1 		= shift;
+	my $seq2 		= shift;
+	my $length 		= shift;
+	
+	# XOR values for each transition/transversion
+	# Transitions
+	# A->G = 6
+	# C->T = 23
+	# Transversions
+	# A->C = 2
+	# A->T = 21
+	# C->G = 4
+	# G->T = 19
+	my @unpacked_seq1 = unpack("C*", $$seq1);
+	my @unpacked_seq2 = unpack("C*", $$seq2);
+	my $transitions 		= 0;
+	my $transversions 		= 0;
+	my $num_comparisons 	= 0;
+	my $pair = '';
+	for (my $i = 0; $i < $length; $i++) {
+		if ($unpacked_seq1[$i] == $unpacked_seq2[$i]) {
+			$num_comparisons++;
+		} else {
+			$pair = $unpacked_seq1[$i]^$unpacked_seq2[$i];
+			# Transitions
+			if($pair 		== 6) { # A->G
+				$transitions++;
+				$num_comparisons++;
+			}
+			elsif($pair 	== 23){ # C->T	
+				$transitions++;
+				$num_comparisons++;
+			} 
+			# Transversions
+			elsif($pair 	== 21){ # A->T
+				$transversions++;
+				$num_comparisons++;
+			}
+			elsif($pair 	== 2){ 	# A->C
+				$transversions++;
+				$num_comparisons++;
+			}
+			elsif($pair 	== 4){ 	# C->G
+				$transversions++;
+				$num_comparisons++;
+			}
+			elsif($pair 	== 19){ # C->T
+				$transversions++;
+				$num_comparisons++;
 			}
 		}
 	}

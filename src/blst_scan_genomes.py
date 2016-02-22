d0cdf74afcea77984d42de624cee46e0dcd276a6
Bryan P. White <bpcwhite@gmail.com>
2016-02-21 18:46:23 -0700

    testing

--- src/blst_scan_genomes.py
+++ src/blst_scan_genomes.py
@@ -23,9 +23,15 @@ import os
 import random
 import subprocess
 import textwrap
+import distance
+import time
+import argparse
+import pickle
 
 ## Local libraries
-from blst_libs import tistamp, load_sequences, rand_subseq
+from blst_libs import tistamp, load_sequences, rand_subseq, sl_window, similar
+
+start_time = time.time()
 
 query_file = "query_targets.txt"
 db_file = "target_genomes.txt"
@@ -33,15 +39,42 @@ db_file = "target_genomes.txt"
 output_path = "~/data_analysis/data/genome_assemblies/"
 blast_path = "~/data_analysis/apps/ncbi-blast-2.2.31+/bin/"
 seq_type = "blastn"
+'''
+parser = argparse.ArgumentParser(description='Scan genomes')
+parser.add_argument('--sum', dest='accumulate',
+                   help='sum the integers (default: find the max)')
+'''
+#args = parser.parse_args()
 
 random_scan = 1
 blast_scan = 0
 mem_scan = 1
-r_scan_size = 500
-r_scan_n = 10
+r_scan_size = 11
+sl_window_size = 11
+r_scan_n = 500
 merge_contigs = 1
-seq_line_limit = 100
+seq_line_limit = 9999999999
+sl_increment = int(0.75*sl_window_size)
+dist_cutoff = 2
+random_pairwise = 0
+sliding_window = 1
+expand_size = 10
+#pickle_cache = 'build'
+clear_cache = 0
 
+#pickle_cache = 1
+
+print(random_scan)
+print(blast_scan)
+print(mem_scan)
+print(r_scan_size)
+print(r_scan_n)
+print(merge_contigs)
+print(seq_line_limit)
+print(sl_window_size)
+print(sl_increment)
+print(dist_cutoff)
+print("Cache: "+ str(clear_cache))
 '''
 -word_size <Integer, >=4>
    Word size for wordfinder algorithm (length of best perfect match)
@@ -75,6 +108,7 @@ for query_file in queries:
 	# Begin analysis of query file
 	print(tistamp(1)+"\tLoading query file: "+query_file)
 
+
 	# Determine query file path
 	split_query = query_file.split('/')
 	query_file_name = split_query[-1]
@@ -85,81 +119,156 @@ for query_file in queries:
 	# Load sequences into dictionary
 	print(tistamp(1)+"\tLoading sequences...")
 	query_sequences = {}
-	query_sequences = load_sequences(query_file, merge_contigs, seq_line_limit)
+
+	pickle_query_file = os.path.expanduser(query_file+".pickle")
+	if clear_cache == 1:
+		if os.path.exists(pickle_query_file):
+			print(tistamp(1)+"\tClearing Cache..." + pickle_query_file)
+			os.unlink(pickle_query_file)
+	if os.path.exists(pickle_query_file):
+		print(tistamp(1)+"\tLoading Cache..." + pickle_query_file)
+		query_sequences = pickle.load( open( pickle_query_file, "rb" ) )
+	else:
+		print(tistamp(1)+"\tBuilding Cache..." + pickle_query_file)
+		query_sequences = load_sequences(query_file, merge_contigs, seq_line_limit)
+		pickle.dump(query_sequences, open( pickle_query_file, "wb" ) )
+
 	print(tistamp(1)+"\tLoaded "+str(len(query_sequences))+" sequences")
+	# Access first sequence in dict (should be merged)
+	query_sequence = next (iter (query_sequences.values()))
 
-	#r_scan_sub_file = os.path.expanduser(output_path+"r_scan_contigs.fas")
-	r_scan_sub_file = query_path+"/r_scan_contigs.fas"
-	scan_file = query_file # Actual file that will be blasted against the database
-	if(random_scan == 1 and merge_contigs == 1 and blast_scan == 1):
-		# Access first sequence in dict (should be merged)
-		query_sequence = next (iter (query_sequences.values()))
-		print(tistamp(1)+"\tGenerating "+str(r_scan_n)+" slices of size "+str(r_scan_size)+"...")
-		r_scan_f = open(os.path.expanduser(r_scan_sub_file), 'w')
-		for i in range(0,r_scan_n):
-			sub_seq = rand_subseq(query_sequence, r_scan_size)
-			r_scan_f.write(">Sub" + str(i) + "\n" + sub_seq + "\n")
-		print(tistamp(1)+"\tPrinted "+str(r_scan_n)+" random sub-sequences to "+r_scan_sub_file)
-		r_scan_f.close()
-		# Set query file to sub sequence file
-		scan_file = r_scan_sub_file
+	print(tistamp(1)+"\tGenerating "+str(r_scan_n)+" slices of size "+str(r_scan_size)+"...")
+	# Generate random query sequences
+	query_seqs = {}
+	#if(random_pairwise == 1):
+	for i in range(0,r_scan_n):
+		(sub_seq,q_pos) = rand_subseq(query_sequence, r_scan_size)
+		query_seqs[q_pos] = sub_seq
 
-		for database in databases:
-			database = ''.join(database[0])
-			# Determine database name
-			split_database = database.split('/')
-			database_file_name = split_database[-1]
-			split_database.pop()
-			database_path = '/'.join(split_database)
-
-			# format output path
-			print(tistamp(1)+"\tTargeting "+database+" with ")
-			blst_results_file = query_path+"/qry_"+query_file_name+"_db_"+database_file_name+"_results_"+tistamp(2)+".tsv"
-			print(tistamp(1)+"\tOutputing BLAST results to "+blst_results_file)
-			# Form blastn command
-			scandb_command = blast_path + seq_type + " -query " + scan_file + " -db " + ''.join(database) +\
-				" -outfmt \"6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore\"" +\
-				" -out " + blst_results_file
-			print(tistamp(1)+"\tRunning: ")
-			print(tistamp(1)+"\t"+scandb_command)
-			#sts = subprocess.Popen(scandb_command, shell=True).wait()
-			os.system(scandb_command)
+	print(tistamp(1)+"\tCreated "+str(r_scan_n)+" random sub-sequences.")
+	#elif(sliding_window == 1):
+		#query_seqs = sl_window(query_sequence, sl_window_size, sl_increment)
 
 
+	for database in databases:
+		database = ''.join(database[0])
 
-	if(random_scan == 1 and mem_scan == 1 and merge_contigs == 1):
-		# Access first sequence in dict (should be merged)
-		query_sequence = next (iter (query_sequences.values()))
-		print(tistamp(1)+"\tGenerating "+str(r_scan_n)+" slices of size "+str(r_scan_size)+"...")
-
-		# Generate random query sequences
-		rand_query_seqs = {}
-		for i in range(0,r_scan_n):
-			(sub_seq,start,end) = rand_subseq(query_sequence, r_scan_size)
-			midpoint = start + int(start-end)/2
-			rand_query_seqs[i] = sub_seq
-
-		print(tistamp(1)+"\tCreated "+str(r_scan_n)+" random sub-sequences.")
-
-		for database in databases:
-			database = ''.join(database[0])
-			# Determine database name
-			split_database = database.split('/')
-			database_file_name = split_database[-1]
-			split_database.pop()
-			database_path = '/'.join(split_database)
-			print(database)
-			# Load database sequences
-			db_sequences = {}
+		print(tistamp(1)+"\tLoading database: "+ database)
+		# Load database sequences
+		db_sequences = {}
+		pickle_db_file = os.path.expanduser(database+".pickle")
+		if clear_cache == 1:
+			if os.path.exists(pickle_db_file):
+				print(tistamp(1)+"\tClearing Cache..." + pickle_db_file)
+				os.unlink(pickle_db_file)
+		if os.path.exists(pickle_db_file):
+			print(tistamp(1)+"\tLoading Cache..." + pickle_db_file)
+			db_sequences = pickle.load( open( pickle_db_file, "rb" ) )
+		else:
+			print(tistamp(1)+"\tBuilding Cache..." + pickle_db_file)
 			db_sequences = load_sequences(database, merge_contigs, seq_line_limit)
-			db_sequence = next (iter (db_sequences.values()))
+			pickle.dump(db_sequences, open( pickle_db_file, "wb" ) )
 
-			rand_db_seqs = {}
-			for i in range(0,r_scan_n):
-				(sub_seq,start,end) = rand_subseq(db_sequence, r_scan_size)
-				rand_db_seqs[i] = sub_seq
+		db_sequence = next (iter (db_sequences.values()))
 
-		''' DIST '''
-		'''
-		ranmem
-		'''
+		db_window_seqs = {}
+		db_window_seqs = sl_window(db_sequence, sl_window_size, sl_increment)
+		print(tistamp(1)+"\tCreated "+str(len(db_window_seqs))+" window sequences.")
+
+		search_i = 1
+		found_seqs = {}
+		while True:
+			print(tistamp(1)+"\tSEARCH ROUND: " + str(search_i))
+
+			dup_hits_i = 0
+			hits_i = 0
+			dup_hits_list = []
+
+			if search_i > 1:
+				print(tistamp(1)+"\tRefining query sequences from: " + str(len(found_seqs)))
+				current_expand_size = expand_size*search_i
+				current_window_size = current_expand_size*2
+				current_window_increment = int(0.75*current_window_size)
+				current_dist_cutoff = int(current_window_size*0.25)
+				print(tistamp(1)+"\tExpanding: "+ str(current_expand_size))
+				print(tistamp(1)+"\tWindow Size: " + str(current_window_size))
+				print(tistamp(1)+"\tWindow Increment: " + str(current_window_increment))
+
+				query_seqs = {}
+				for q_pos, d_pos in found_seqs.items():
+					q_left = q_pos - current_expand_size
+					q_right = q_pos + current_expand_size
+					query_seqs[q_left] = query_sequence[q_left:q_right]
+					#print(query_sequence[q_left:q_right])
+					#exit()
+				# Validate window Size
+				if current_window_size != current_expand_size:
+					print(tistamp(1)+"\tQuery sequence size does not match window size.")
+				# Clear previously found sequences
+				found_seqs = {}
+				# Generate new window seqs based on size expand
+				db_window_seqs = sl_window(db_sequence, current_window_size, current_window_increment)
+				print(tistamp(1)+"\tCreated "+str(len(db_window_seqs))+" window sequences.")
+
+			print(tistamp(1)+"\tScanning Windows...")
+
+			# Query dict
+			min_dist = 99999999
+			q_scan_i = 0
+			q_scan_max = len(query_seqs)
+			q_scan_inc = int(q_scan_max * 0.01)
+			#print(tistamp(1))
+			for q_pos, q_seq in query_seqs.items():
+				found_hit = 0
+				'''
+				if (q_scan_i % q_scan_inc) == 0:
+					pcnt_complete = int(q_scan_i/q_scan_max)*100
+					print("=", end="")
+				'''
+				# Database dict
+				for d_pos, d_seq in db_window_seqs.items():
+					ldist = ''
+
+					if search_i > 1:
+						ldist = distance.levenshtein(q_seq,d_seq)
+					else:
+						ldist = distance.fast_comp(q_seq,d_seq)
+
+					if ldist < min_dist:
+						min_dist = ldist
+
+					if ldist != -1 and ldist < dist_cutoff:
+						hits_i = hits_i + 1
+						found_hit = found_hit + 1
+						found_seqs[q_pos] = d_pos
+						# Skip seqs matching more than 1 place in the target genome
+						if found_hit > 1:
+							dup_hits_i = dup_hits_i + 1
+							dup_hits_list.append(q_pos)
+							continue
+				q_scan_i = q_scan_i + 1
+			print(tistamp(1)+"\tMinimum distance encountered: " + str(min_dist))
+			for dup_q_pos in dup_hits_list:
+				if dup_q_pos in found_seqs:
+					del found_seqs[dup_q_pos]
+
+			print("Found " + str(hits_i)+ " hits")
+			print("Saved " + str(len(found_seqs)) + " hits")
+			print("Duplicate hits: " + str(dup_hits_i))
+			if search_i > 1:
+				for q_pos, d_pos in found_seqs.items():
+						print(str(q_pos) + " \t " \
+						+ str(d_pos) + " \t " \
+						+ str(query_seqs[q_pos]) + " \t " \
+						+ db_window_seqs[d_pos])
+
+			# Iterate number of search rounds
+			search_i = search_i + 1
+			if len(found_seqs) == 0:
+				break
+
+
+
+
+end_time = time.time()
+print("Time: " + str(end_time - start_time))
